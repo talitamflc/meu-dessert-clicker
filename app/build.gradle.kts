55f4011e400a4f0dc1d01ac3c6d79af81e71d611
tmoraisflc <tmoraisfl@gmail.com>
2026-07-16 12:10:45 -0300

    removed dependency duplicate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${lifecycleVersion}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
